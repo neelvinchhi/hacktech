49592ccc3ba8fb9fbbe3fbf68f654d94a7b2bff7
Shamant <74949605+Shamant@users.noreply.github.com>
2024-04-20 13:59:00 +0530

    Create App.js

---
+++ frontend/app/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GoogleAuth from './GoogleAuth';
+
+function App() {
+  return (
+    <div className="App">
+      <GoogleAuth />
+    </div>
+  );
+}
+
+export default App;
