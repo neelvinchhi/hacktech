31e6b1c579dae4df77fc6e8fa2a1ef1a8d534ffe
Neel Vinchhi <neelvinchhi2007@gmail.com>
2024-04-20 18:00:45 +0530

    google auth works

--- frontend/app/GoogleAuth.js
+++ frontend/app/GoogleAuth.js
@@ -1,9 +1,11 @@
 "use client"
 
-import React from 'react';
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import { React, useState } from 'react';
+import * as firebase from 'firebase/app';
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyCz0uAMDnDk7T0v9BYV6ueIwp8zJGn0f10",
@@ -15,41 +17,29 @@ const firebaseConfig = {
   measurementId: "G-0RRF2PHJC2"
 };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 const GoogleAuth = () => {
-  const handleSignUpWithGoogle = async () => {
-    try {
-      const provider = new firebase.auth.GoogleAuthProvider();
-      const result = await firebase.auth().signInWithPopup(provider);
-      const user = result.user;
-      console.log('Signed up user:', user);
-      alert('Signed up successfully!');
-    } catch (error) {
-      console.error('Error signing up with Google:', error.message);
-      alert('Error signing up with Google. Please try again.');
-    }
-  };
+  const [error, setError] = useState(null);
 
-  const handleLogInWithGoogle = async () => {
+  const handleSignInWithGoogle = async () => {
     try {
-      const provider = new firebase.auth.GoogleAuthProvider();
-      const result = await firebase.auth().signInWithPopup(provider);
-      const user = result.user;
-      console.log('Logged in user:', user);
-      alert('Logged in successfully!');
+      const provider = new GoogleAuthProvider();
+      await signInWithPopup(auth, provider);
+      console.log('Signed in successfully!');
+      // Add any additional logic after successful sign-in
     } catch (error) {
-      console.error('Error logging in with Google:', error.message);
-      alert('Error logging in with Google. Please try again.');
+      console.error('Error signing in with Google:', error.message);
+      setError('Error signing in with Google. Please try again.');
     }
   };
 
   return (
     <div>
       <h1>Firebase Authentication Demo</h1>
-      <button onClick={handleSignUpWithGoogle}>Sign Up with Google</button>
-      <button onClick={handleLogInWithGoogle}>Log In with Google</button>
+      <button onClick={handleSignInWithGoogle}>Sign In with Google</button>
+      {error && <p>{error}</p>}
     </div>
   );
 };
