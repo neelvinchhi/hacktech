f70211787aa6b1b385160e5011a2bff43a95b256
Shamant <74949605+Shamant@users.noreply.github.com>
2024-04-21 14:35:43 +0530

    Update page.js

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,12 +1,19 @@
+import React from 'react';
 import GoogleAuth from "./googleauth";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Preview from 'dashboard/page';
+import Chat from './chat';
 
 const App = () => {
   return (
     <>
-      <GoogleAuth /> 
+      <GoogleAuth />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Preview} />
+          <Route path="/chat" component={Chat} />
+        </Switch>
+      </Router>
     </>
-  
-    )
-  }
-
-export default App;
+  );
+}
