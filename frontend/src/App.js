bea70d26bd8ace4f07e5b6c9b47233b6471b90f4
Neel Vinchhi <neelvinchhi2007@gmail.com>
2024-04-21 18:32:30 +0530

    working UI for quiz

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import Preview from './preview';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Form } from 'react-router-dom';
 import Chat from './chat';
+import QuizComponent from './quiz';
 
 const App = () => {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route exact path="/" element={<Preview />} />
-          <Route path="/chat" element={<Chat />} />
-        </Routes>
-      </div>
-    </Router>
+    // <Router>
+    //   <div className="App">
+    //     <Routes>
+    //       <Route exact path="/" element={<Preview />} />
+    //       <Route path="/chat" element={<Chat />} />
+
+    //     </Routes>
+    //   </div>
+    // </Router>
+    <QuizComponent />
   );
 };
 
